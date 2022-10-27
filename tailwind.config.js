3c75edc2fc479c3384795be5d0b1d0b4973d6991
Alec Reimel <alecreimel1@gmail.com>
2022-10-27 11:37:24 -0400

    tailwind config updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,69 @@
-/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme')
+const config = require('./tailwind.theme.config')
+/**
+ * Find the applicable theme color palette, or use the default one
+ */
+const themeConfig = process.env.THEME_KEY && config[process.env.THEME_KEY] ? config[process.env.THEME_KEY] : config.default
+const { colors } = themeConfig
 module.exports = {
-	content: ["./src/**/*.{astro,html,jsx,tsx,svelte,vue,js,ts}"],
-	theme: {
+    darkMode: 'class',
+    content: [
+        './public/**/*.html',
+        './src/**/*.{astro,js,ts}'
+    ],
+    safelist: ['dark'],
+    theme: {
+		fontFamily: {
+			sans: ['Fira Code', ...fontFamily.sans],
+		},
 		extend: {
-			screens: {
-				sm: "400px",
-			},
+            colors: {
+                theme: {
+                    ...colors
+                }
+            },
+            typography: (theme) => ({
+                dark: {
+                    css: {
+                        color: theme("colors.gray.200"),
+                    },
+                },
+                DEFAULT: {
+                    css: {
+                        a: {
+                            color: colors.dark.primary,
+                              '&:hover': {
+                                color: colors.primary,
+                              },
+                        },
+                        blockquote: {
+                            color: colors.primary,
+                            borderColor: colors.dark.primary
+                        },
+                        'blockquote > p::before, p::after': {
+                            color: colors.dark.primary,
+                        },
+                        h1: {
+                            color: colors.dark.secondary,
+                        },
+                        h2: {
+                            color: colors.dark.secondary,
+                        },
+                        h3: {
+                            color: colors.dark.secondary,
+                        },
+                    }
+                },
+            }),
 		},
 	},
-	plugins: [],
+    variants: {
+        extend: { typography: ["dark"] }
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio'),
+    ]
 };
